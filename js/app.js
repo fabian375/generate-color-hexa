a65c67dfbe50183d23b26398a3152024efd2dd5c
Ameneiros <itzuitzu2@gmail.com>
2020-12-22 12:27:49 -0300

    1

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function changeBackground(){
+    const symbols="0123456789ABCDEF";
+    let color="#";
+
+    for(let i=0;i<6;i++){
+        color=color+symbols[Math.floor(Math.random()*16)];
+
+    }
+    document.body.style.background=color;
+    document.getElementById("hexa").innerHTML=color;
+}
